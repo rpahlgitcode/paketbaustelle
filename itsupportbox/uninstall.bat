1601e763e96fd5c2d86d825e168942a609102d16
rpahlgitcode <rolandpahl@live.de>
2026-03-07 12:23:14 +0100

    Delete itsupportbox directory
    nicht mehr benoetigt, das Setup erstellt

--- itsupportbox/uninstall.bat
+++
@@ -1,6 +0,0 @@
-start /wait taskkill /IM itsupportbox.exe /F 
-rmdir /s /q "C:\Program Files\itsupportbox\" 
-del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\IT Support Box.lnk" 
-del "C:\Users\Public\Desktop\IT Support Box.lnk"
-del "C:\ProgramData\softlogs\Logs\itsbox_install_ok.txt"  
-timeout 15
